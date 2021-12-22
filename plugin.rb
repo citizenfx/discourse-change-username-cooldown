0165b5b91fec1baf80a6772fc821503d1cfc0ba2
Jonmichael Chambers <jon@tyrbo.net>
2021-12-22 02:47:54 -0700

    initial commit

---
+++ plugin.rb
@@ -0,0 +1,64 @@
+# name: discourse-change-username-cooldown
+# about: Allows username change on a cooldown.
+# version: 0.0.1
+
+enabled_site_setting :change_username_enabled
+
+after_initialize do
+    require 'action_view'
+
+    UserGuardian.module_eval do
+        original_can_edit_username = instance_method(:can_edit_username?)
+
+        define_method(:can_edit_username?) do |user|
+            original_result = original_can_edit_username.bind(self).(user)
+
+            return original_result unless SiteSetting.change_username_enabled
+
+            unless original_result
+                return false unless is_me?(user)
+
+                last_changed = user.user_custom_fields.find_by(name: 'username_changed_at')&.value
+
+                if last_changed.nil?
+                    return true
+                else
+                    last_changed = DateTime.parse(last_changed)
+                    cooldown = SiteSetting.change_username_cooldown.days
+
+                    return Time.current >= last_changed + cooldown
+                end
+            end
+
+            original_result
+        end
+    end
+
+    add_to_serializer(:user, :username_changed_at) do
+        object.user_custom_fields.find_by(name: 'username_changed_at')&.value
+    end
+
+    add_to_serializer(:user, :username_change_available_in) do
+        last_changed = object.user_custom_fields.find_by(name: 'username_changed_at')&.value
+
+        unless last_changed.nil?
+            last_changed = DateTime.parse(last_changed)
+            cooldown = SiteSetting.change_username_cooldown.days
+
+            if Time.current < last_changed + cooldown
+                return Class.new.extend(ActionView::Helpers::DateHelper).time_ago_in_words(last_changed + cooldown)
+            end
+        end
+    end
+
+    on(:username_changed) do |old_username, new_username|
+        return unless SiteSetting.change_username_enabled
+
+        user = User.find_by(username: new_username)
+
+        user.user_custom_fields.find_or_initialize_by(name: 'username_changed_at').tap do |custom_field|
+            custom_field.value = Time.current
+            custom_field.save
+        end
+    end
+end
---
+++ spec/integration/cooldown_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Cooldown' do
+    let(:original_change_period) { SiteSetting.username_change_period }
+    let(:time) { Time.current }
+
+    before do
+        SiteSetting.username_change_period = 0
+    end
+
+    after do
+        SiteSetting.username_change_period = original_change_period
+    end
+
+    describe 'when logged in' do
+        let(:user) { Fabricate(:user, created_at: time - 1.year) }
+        let(:user2) { Fabricate(:user, created_at: time - 1.year) }
+
+        before do
+            sign_in(user)
+        end
+
+        it 'cant change someone elses username' do
+            get "/u/#{user2.username}.json"
+
+            body = JSON.parse(response.body)
+
+            expect(body['user']['can_edit_username']).to be(false)
+        end
+
+        describe 'when change username is available' do
+            before do
+                user.user_custom_fields.create(name: 'username_changed_at', value: time - SiteSetting.change_username_cooldown.days)
+            end
+
+            it 'should return proper payload' do
+                freeze_time(time) do
+                    get "/u/#{user.username}.json"
+
+                    body = JSON.parse(response.body)
+
+                    expect(body['user']['can_edit_username']).to be(true)
+                end
+            end
+        end
+
+        describe 'when change username is on cooldown' do
+            before do
+                user.user_custom_fields.create(name: 'username_changed_at', value: time)
+            end
+
+            it 'should return the proper payload' do
+                get "/u/#{user.username}.json"
+
+                body = JSON.parse(response.body)
+
+                expect(body['user']['can_edit_username']).to be(false)
+                expect(body['user']['username_changed_at']).to eq(time.to_s)
+                expect(body['user']['username_change_available_in']).to eq('1y')
+            end
+        end
+    end
+end
