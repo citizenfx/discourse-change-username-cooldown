837631a52ca604f3c14de7cca9cae53f43ffc4d0
Jonmichael Chambers <jon@tyrbo.net>
2022-03-04 21:18:00 -0700

    revert back to module_eval/class_eval

--- plugin.rb
+++ plugin.rb
@@ -4,49 +4,49 @@
 
 enabled_site_setting :change_username_enabled
 
-module UsernameChangerPatch
-    def update_username(attrs = {})
-        return super unless SiteSetting.change_username_enabled
-
-        user = User.find(attrs[:user_id])
-        user.user_custom_fields.find_or_initialize_by(name: 'username_changed_at').tap do |custom_field|
-            custom_field.value = Time.current.iso8601
-            custom_field.save
-        end
-
-        super
-    end
-end
-
-module UserGuardianPatch
-    def can_edit_username?(user)
-        original_result = super
-
-        return original_result unless SiteSetting.change_username_enabled
-
-        unless original_result
-            return false unless is_me?(user)
-
-            last_changed = user.user_custom_fields.find_by(name: 'username_changed_at')&.value
-
-            if last_changed.nil?
-                return true
-            else
-                last_changed = DateTime.parse(last_changed)
-                cooldown = SiteSetting.change_username_cooldown.days
-
-                return Time.current >= last_changed + cooldown
-            end
-        end
-
-        original_result
-    end
-end
-
 after_initialize do
-    UsernameChanger.singleton_class.prepend(UsernameChangerPatch)
+    UsernameChanger.class_eval do
+        original_update_username = singleton_method(:update_username)
 
-    UserGuardian.prepend(UserGuardianPatch)
+        define_singleton_method(:update_username) do |attrs|
+            return original_update_username.call(attrs) unless SiteSetting.change_username_enabled
+
+            user = User.find(attrs[:user_id])
+            user.user_custom_fields.find_or_initialize_by(name: 'username_changed_at').tap do |custom_field|
+                custom_field.value = Time.current.iso8601
+                custom_field.save
+            end
+
+            original_update_username.call(attrs)
+        end
+    end
+
+    UserGuardian.module_eval do
+        original_can_edit_username = instance_method(:can_edit_username?)
+
+        define_method(:can_edit_username?) do |user|
+            original_result = original_can_edit_username.bind(self).(user)
+
+            return original_result unless SiteSetting.change_username_enabled
+
+            unless original_result
+                return false unless is_me?(user)
+
+                last_changed = user.user_custom_fields.find_by(name: 'username_changed_at')&.value
+
+                if last_changed.nil?
+                    return true
+                else
+                    last_changed = DateTime.parse(last_changed)
+                    cooldown = SiteSetting.change_username_cooldown.days
+
+                    return Time.current >= last_changed + cooldown
+                end
+            end
+
+            original_result
+        end
+    end
 
     add_to_serializer(:user, :username_changed_at) do
         object.user_custom_fields.find_by(name: 'username_changed_at')&.value
